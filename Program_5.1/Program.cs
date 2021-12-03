10663eae37d30cf3d818dd2354e0e6f5f894636f
Илья Филиппов <filippov.i.s@gmail.com>
2021-12-03 16:56:53 +0300

    Добавьте файлы проекта.

---
+++ Program_5.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] n = new double[7];
+            double sum = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Введите {0}-й элемент", i + 1);
+                n[i] = Convert.ToDouble(Console.ReadLine());
+                sum += n[i];
+            }
+            int x = 0;
+            double result = sum / n.Length;
+
+            Console.WriteLine();
+            Console.WriteLine("Среднее арифметическое арифметическое элементов = {0:0.00}", result);
+            Console.ReadKey();
+        }
+    }
+}
